cd81b1477625e6412375f15c4414841ec27aa6d1
Cedric Geissmann <cedric.geissmann@gmail.com>
2022-11-21 21:09:15 +0100

    with comment

--- utils.js
+++ utils.js
@@ -2,6 +2,7 @@ function read() {
     return document.querySelector("#input").value
 }
 
+// Added a comment
 function print(text) {
     const output = document.querySelector("#output")
     output.textContent += "\n > " + text
