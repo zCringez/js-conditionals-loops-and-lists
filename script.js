e6c6c78f42edab49179e5da948bdb9f552e4eb41
Miran <e227726@sbl.ch>
2023-02-03 13:54:36 +0100

    Viele Aufgaben

--- script.js
+++ script.js
@@ -50,7 +50,78 @@ function checkForXAufgabe12() {
         }
         console.log(result);
       }
+      //Ist keine  Zeichenkette als bei 14 wäre es 1144 und nicht 1414
+      function countSpacesAufgabe15() {
+        let input = read();
+        let count = 0;
+        for (let i = 0; i < input.length; i++) {
+          if (input[i] === " ") {
+            count++;
+          }
+        }
+        console.log("Anzahl der Leerzeichen: " + count);
+      }
+      function replaceSpacesAufgabe16() {
+        let input = read();
+        let result = "";
+        for (let i = 0; i < input.length; i++) {
+          if (input[i] === " ") {
+            result += "_";
+          } else {
+            result += input[i];
+          }
+        }
+        console.log(result);
+      }
+      function checkPeriodsAufgabe17() {
+        let input = read();
+        let valid = true;
+        for (let i = 0; i < input.length - 1; i++) {
+          if (input[i] === "." && input[i + 1] !== " ") {
+            valid = false;
+            break;
+          }
+        }
+        console.log("Eingabe ist gültig: " + valid);
+      }
       
+      function reverseInputAufgabe18() {
+        let input = read();
+        let result = "";
+        for (let i = input.length - 1; i >= 0; i--) {
+          result += input[i];
+        }
+        console.log(result);
+      }
+      function checkPalindromeAufgabe19() {
+        let input = read();
+        let valid = true;
+        for (let i = 0; i < input.length / 2; i++) {
+          if (input[i] !== input[input.length - i - 1]) {
+            valid = false;
+            break;
+          }
+        }
+        console.log("Eingabe ist ein Palindrom: " + valid);
+      }
+
+      function toUppercaseAufgabe20() {
+        let input = read();
+        let result = "";
+        for (let i = 0; i < input.length; i++) {
+          result += input[i].toUpperCase();
+        }
+        console.log(result);
+      }
+      
+
+      
+      
+      
+      
+      
+    
+
       
 
 
